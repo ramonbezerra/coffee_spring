0afe6a10be479df37a181b66ce88103164242cb3
Ramon Bezerra da Nóbrega <ramon.bezerra@outlook.com>
2020-10-23 20:11:19 -0300

    connecting to database

--- src/main/java/com/example/coffee/Coffee.java
+++ src/main/java/com/example/coffee/Coffee.java
@@ -1,19 +1,35 @@
 package com.example.coffee;
 
-import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "coffee")
 public class Coffee {
-    private final String id;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
 
     public Coffee(String name) {
-        this(UUID.randomUUID().toString(), name);
+        this.name = name;
     }
 }
--- src/main/java/com/example/coffee/CoffeeController.java
+++ src/main/java/com/example/coffee/CoffeeController.java
@@ -2,12 +2,23 @@ package com.example.coffee;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @RestController
 @RequestMapping("/coffees")
@@ -23,15 +34,50 @@ public class CoffeeController {
         ));
     }
 
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
     // @RequestMapping(value = "/coffees", method = RequestMethod.GET)
     @GetMapping
     public Iterable<Coffee> getCoffees() {
-        return coffees;
+        return coffeeRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Coffee> getMethodName(@PathVariable String id) {
+        Optional<Coffee> coffee = coffeeRepository.findById(Long.parseLong(id));
+        
+        if (coffee.isEmpty())
+            return ResponseEntity.notFound().build();
+        
+        return ResponseEntity.ok(coffee.get());
+    }    
+
     @PostMapping
-    public Coffee postCoffee(@RequestBody Coffee coffee) {
-        coffees.add(coffee);
-        return coffee;
+    @ResponseBody
+    public ResponseEntity<Coffee> postCoffee(@RequestBody Coffee coffee) {
+        // coffees.add(coffee);
+        try {
+            return ResponseEntity.ok(coffeeRepository.save(coffee));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        try {
+            return ResponseEntity.ok(coffeeRepository.save(coffee));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCoffee(@PathVariable Long id) {
+        coffeeRepository.delete(coffeeRepository.findById(id).get());
+        return ResponseEntity.noContent().build();
     }
 }
